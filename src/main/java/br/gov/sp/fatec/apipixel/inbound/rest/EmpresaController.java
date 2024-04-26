50cd6d40cf268e2a623e67462c74a3b71f5044e5
41issonm <santos.alisson207230@gmail.com>
2024-04-26 00:23:05 -0300

    feat:criacao do enpoint post

--- src/main/java/br/gov/sp/fatec/apipixel/inbound/rest/EmpresaController.java
+++ src/main/java/br/gov/sp/fatec/apipixel/inbound/rest/EmpresaController.java
@@ -1,12 +1,12 @@
 package br.gov.sp.fatec.apipixel.inbound.rest;
 
+import br.gov.sp.fatec.apipixel.core.domain.entity.Empresa;
 import br.gov.sp.fatec.apipixel.core.domain.projection.EmpresaProjection;
 import br.gov.sp.fatec.apipixel.core.usecase.empresa.CarregarEmpresaUC;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +16,19 @@ import java.util.List;
 public class EmpresaController {
 
     private final CarregarEmpresaUC carregarEmpresaUC;
+    private final CarregarEmpresaUC criarEmpresaUC;
+    private final CarregarEmpresaUC dele;
+
 
     @GetMapping
     public ResponseEntity<List<EmpresaProjection>> findAll(){
         return ResponseEntity.ok(carregarEmpresaUC.executar());
     }
+
+    @PostMapping
+    public ResponseEntity<Empresa> criarEmpresa() {
+        Empresa empresaCriada = (Empresa) criarEmpresaUC.executar();
+        return ResponseEntity.status(HttpStatus.CREATED).body(empresaCriada);
+    }
+
 }
